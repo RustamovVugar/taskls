bb3b5cf9f695abb7017a74df8fe6e8de1f2a14d1
RustamovVugar <vugarar@code.edu.az>
2023-02-05 00:01:35 +0400

    add homeworkjs folder

---
+++ main.js
@@ -0,0 +1,71 @@
+"use strict"
+//task1
+// let num=21;
+
+// if (num%3==0 && num%7==0) {
+//     console.log("eded 3 ve 7 bolunur");
+// }else{
+//     console.log("eded 3 ve 7 bolunmur");
+// }
+
+
+
+//task2
+// let m=4;
+// let n=2;
+// let sum=0;
+
+
+// if (n%2==0 && m%2==0) {
+//     sum=n+m;
+//     console.log(sum);
+// }else{
+//     console.log("cut ededler deyil");
+// }
+
+
+
+//task3
+// let n=3;
+// let m=25;
+// let count=0;
+
+// for (let i = n; i <= m; i++) {
+//     if (i%2==1) {
+//         count++;
+       
+//     }
+ 
+// }
+// console.log(count);  
+
+
+
+//task4
+// let n=3;
+// let m=9;
+// let sum=0;
+
+// for (let i = n; i <= m; i++) {
+//     if (i%2==1) {
+//         sum=sum+i;
+       
+//     }
+ 
+// }
+// console.log(sum);  
+
+
+
+//taks5
+// let n=3;
+// let m=9;
+
+
+// for (let i = n; i <= m; i++) {
+//     if (i%2==1) {
+//         console.log(i);  
+//     }
+// }
+
+
